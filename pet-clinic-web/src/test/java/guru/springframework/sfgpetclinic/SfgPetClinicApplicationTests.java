77983b87c740a340b8d786f0801c5af4e95f9c39
jidelson <joeidelson@gmail.com>
2022-07-15 13:03:30 -0500

    modifying pom

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,9 +1,10 @@
-package sfgpetclinic;
+package guru.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+
 class SfgPetClinicApplicationTests {
 
     @Test
