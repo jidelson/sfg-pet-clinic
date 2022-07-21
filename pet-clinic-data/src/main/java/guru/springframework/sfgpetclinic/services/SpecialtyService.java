b356ab0cc7da8df2f0aec034663ea94bbe1a637e
jidelson <joeidelson@gmail.com>
2022-07-21 10:39:12 -0500

    added Specialties to Vets on startup. closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtyService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Specialty;
 
-public interface SpecialtiesService extends CrudService<Specialty, Long>{
+public interface SpecialtyService extends CrudService<Specialty, Long>{
 
 
 }
